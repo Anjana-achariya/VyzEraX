67c49721d073a31e6e02847f50b50243458b1b0c
Anjana-achariya <131129025+Anjana-achariya@users.noreply.github.com>
2025-12-22 22:40:43 +0530

    Update analyse.js

--- src/api/analyse.js
+++ src/api/analyse.js
@@ -1,9 +1,26 @@
+// export async function analyseDataset(file) {
+//   const formData = new FormData();
+//   formData.append("file", file);
+
+//   const response = await fetch("http://localhost:8000/api/analyse", {
+
+//     method: "POST",
+//     body: formData,
+//   });
+
+//   if (!response.ok) {
+//     throw new Error("Analysis failed");
+//   }
+
+//   return await response.json();
+// }
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export async function analyseDataset(file) {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("http://localhost:8000/api/analyse", {
-
+  const response = await fetch(`${API_BASE_URL}/api/analyse`, {
     method: "POST",
     body: formData,
   });
@@ -14,3 +31,4 @@ export async function analyseDataset(file) {
 
   return await response.json();
 }
+
